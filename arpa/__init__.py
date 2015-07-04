21ec94c71b9a62961e3fe18ec5caa84459157adf
Stefan Fischer <sfischer13@ymail.com>
2015-07-04 15:45:37 +0200

    setup

---
+++ arpa/__init__.py
@@ -0,0 +1,43 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 Stefan Fischer
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""
+This is a library for reading ARPA n-gram models.
+
+It may be imported directly:
+
+    import arpa
+
+The library was initiated by Stefan Fischer and is developed and maintained by many others.
+"""
+
+__all__ = []
+
+__author__ = 'Stefan Fischer'
+__author_email__ = 'sfischer13@ymail.com'
+__authors__ = ['Stefan Fischer']
+__copyright__ = 'Copyright (c) 2015 Stefan Fischer'
+__credits__ = []
+__date__ = '2015-07-04'
+__license__ = 'MIT'
+__status__ = 'development'
+__url__ = 'https://github.com/sfischer13/python-arpa/'
+__version__ = '0.1.0'
