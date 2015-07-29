ce2a8b682eea0396da324f2d54d278fd8e18f39b
Stefan Fischer <sfischer13@ymail.com>
2015-07-29 22:21:03 +0200

    new test for lm.counts()

--- tests/test_model_simple.py
+++ tests/test_model_simple.py
@@ -1,6 +1,10 @@
 from arpa.models.simple import ARPAModelSimple
 
 
+def test_new_model_counts():
+    lm = ARPAModelSimple()
+    assert lm.counts() == []
+
 def test_new_model_len():
     lm = ARPAModelSimple()
     assert len(lm) == 0
