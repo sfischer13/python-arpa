85f7bfa1c1822a493d45526a672874036c7ffa2f
Stefan Fischer <sfischer13@ymail.com>
2015-08-10 21:14:48 +0200

    test for arpa.load + arpa.dump

--- tests/test_arpa.py
+++ tests/test_arpa.py
@@ -8,6 +8,16 @@ import arpa
 TEST_ARPA = os.path.join(os.path.dirname(__file__), "data/test.arpa")
 
 
+def test_load_dump():
+    with open(TEST_ARPA, "rt") as fp:
+        lm = arpa.load(fp)[0]
+        fp.seek(0)
+        with tempfile.TemporaryFile(mode="w+t") as gp:
+            arpa.dump(lm, gp)
+            gp.seek(0)
+            assert fp.read() == gp.read()
+
+
 def test_loadf_dumpf():
     lm = arpa.loadf(TEST_ARPA)[0]
     out = tempfile.NamedTemporaryFile(mode="w+t", delete=False)
