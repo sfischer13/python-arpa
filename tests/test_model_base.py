35d1e872876e184cbdc8f501dd16b4a8b563f674
Stefan Fischer <sfischer13@ymail.com>
2015-08-12 19:56:17 +0200

    test for lm.counts()

--- tests/test_model_base.py
+++ tests/test_model_base.py
@@ -4,3 +4,8 @@ from arpa.models.simple import ARPAModelSimple
 def test_new_model_contains_not():
     lm = ARPAModelSimple()
     assert "foo" not in lm
+
+
+def test_new_model_counts():
+    lm = ARPAModelSimple()
+    assert lm.counts() == []
