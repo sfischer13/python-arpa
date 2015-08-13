3e0021ffd9dd2aaea68f8e442a0d3067c1ecf525
Stefan Fischer <sfischer13@ymail.com>
2015-08-14 00:39:40 +0200

    test for len(lm)

--- tests/test_model_base.py
+++ tests/test_model_base.py
@@ -9,3 +9,8 @@ def test_new_model_contains_not():
 def test_new_model_counts():
     lm = ARPAModelSimple()
     assert lm.counts() == []
+
+
+def test_new_model_len():
+    lm = ARPAModelSimple()
+    assert len(lm) == 0
