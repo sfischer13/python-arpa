8b37192077ff898cddd0888b451ee7dbc297cdaf
Stefan Fischer <sfischer13@ymail.com>
2015-09-11 20:53:07 +0200

    tests for lm.log_p with bad input

--- tests/test_model_base.py
+++ tests/test_model_base.py
@@ -22,3 +22,21 @@ def test_log_p_raw():
     lm = ARPAModelSimple()
     with pytest.raises(KeyError):
         lm.log_p_raw("UnladenSwallow")
+
+
+def test_log_p_empty_string():
+    lm = ARPAModelSimple()
+    with pytest.raises(ValueError):
+        lm.log_p("")
+
+
+def test_log_p_empty_tuple():
+    lm = ARPAModelSimple()
+    with pytest.raises(ValueError):
+        lm.log_p(tuple())
+
+
+def test_log_p_int():
+    lm = ARPAModelSimple()
+    with pytest.raises(ValueError):
+        lm.log_p(1)
