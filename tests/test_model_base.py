68b4824689e5e1920ac8abdeb321049b2ac7682e
Stefan Fischer <sfischer13@ymail.com>
2015-09-11 20:39:25 +0200

    test for model.log_p_raw(ngram)

--- tests/test_model_base.py
+++ tests/test_model_base.py
@@ -1,3 +1,5 @@
+import pytest
+
 from arpa.models.simple import ARPAModelSimple
 
 
@@ -14,3 +16,9 @@ def test_new_model_counts():
 def test_new_model_len():
     lm = ARPAModelSimple()
     assert len(lm) == 0
+
+
+def test_log_p_raw():
+    lm = ARPAModelSimple()
+    with pytest.raises(KeyError):
+        lm.log_p_raw("UnladenSwallow")
