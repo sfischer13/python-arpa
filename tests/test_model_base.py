a3cd7f05c917ef88a500194d6a234d898ee5ef99
Stefan Fischer <sfischer13@ymail.com>
2015-09-11 21:13:54 +0200

    test for lm.p(ngram) and lm.log_p(ngram) using values from test.arpa

--- tests/test_model_base.py
+++ tests/test_model_base.py
@@ -1,7 +1,20 @@
 import pytest
 
+import arpa
 from arpa.models.base import ARPAModel
 from arpa.models.simple import ARPAModelSimple
+
+from test_arpa import TEST_ARPA
+
+
+def test_manual_log_p_unk():
+    lm = arpa.loadf(TEST_ARPA)[0]
+    assert lm.log_p("UnladenSwallow") == -1.995635
+
+
+def test_manual_p():
+    lm = arpa.loadf(TEST_ARPA)[0]
+    assert round(lm.p("<s>"), 4) == 0
 
 
 def test_new_model_contains_not():
@@ -38,6 +51,12 @@ def test_log_p_empty_tuple():
 
 
 def test_log_p_int():
+    lm = ARPAModelSimple()
+    with pytest.raises(ValueError):
+        lm.log_p(1)
+
+
+def test_log_s_int():
     lm = ARPAModelSimple()
     with pytest.raises(ValueError):
         lm.log_p(1)
