c8accae8835847a5027f2760399f900f714d6295
Stefan Fischer <sfischer13@ymail.com>
2015-09-11 20:25:52 +0200

    test for parse(fp) function

---
+++ tests/test_parser_base.py
@@ -0,0 +1,11 @@
+from arpa.models.base import ARPAModel
+from arpa.models.simple import ARPAModelSimple
+from arpa.parsers.quick import ARPAParserQuick
+
+from test_arpa import TEST_ARPA
+
+
+def test_parse():
+    with open(TEST_ARPA, "rt") as fp:
+        result = ARPAParserQuick(ARPAModelSimple).parse(fp)[0]
+        assert isinstance(result, ARPAModel)
