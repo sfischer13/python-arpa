f4f3e2ca0f0d9b1438665fd2615579360e945367
Stefan Fischer <sfischer13@ymail.com>
2015-09-11 20:58:17 +0200

    test for lm._check_input(input)

--- tests/test_model_base.py
+++ tests/test_model_base.py
@@ -1,5 +1,6 @@
 import pytest
 
+from arpa.models.base import ARPAModel
 from arpa.models.simple import ARPAModelSimple
 
 
@@ -40,3 +41,21 @@ def test_log_p_int():
     lm = ARPAModelSimple()
     with pytest.raises(ValueError):
         lm.log_p(1)
+
+
+def test_check_input_list():
+    lm = ARPAModelSimple()
+    result = ARPAModel._check_input(["foo", "bar"])
+    assert isinstance(result, tuple)
+
+
+def test_check_input_string_word():
+    lm = ARPAModelSimple()
+    result = ARPAModel._check_input("foo")
+    assert isinstance(result, tuple) and len(result) == 1
+
+
+def test_check_input_string_words():
+    lm = ARPAModelSimple()
+    result = ARPAModel._check_input("foo bar")
+    assert isinstance(result, tuple) and len(result) == 2
