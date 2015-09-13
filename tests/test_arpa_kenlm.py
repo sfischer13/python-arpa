e8c1aec354bbd7ff18d1cb12ff9a7ef80fc10116
Stefan Fischer <sfischer13@ymail.com>
2015-09-13 16:25:44 +0200

    new test for lm.log_s

--- tests/test_arpa_kenlm.py
+++ tests/test_arpa_kenlm.py
@@ -3,7 +3,7 @@ import os
 import random
 import sys
 
-from nltk.corpus import PlaintextCorpusReader
+import nltk
 
 import kenlm
 
@@ -11,14 +11,14 @@ import arpa
 
 MAX_ORDER = 5
 N_QUERIES = 10
+N_SENTENCES = 10
 
 TEST_ARPA = os.path.join(os.path.dirname(__file__), "data/test.arpa")
-WORDS = list(PlaintextCorpusReader("/usr/share/dict", "words").words())
+WORDS = list(nltk.corpus.PlaintextCorpusReader("/usr/share/dict", "words").words())
 
 
 def test_log_p_random():
     random_queries = list(_random_queries())
-    print(random_queries, file=sys.stderr)
     _test_log_p(random_queries)
 
 
@@ -43,3 +43,38 @@ def _random_queries():
     for order in range(1, MAX_ORDER + 1):
         for _ in range(N_QUERIES):
             yield _random_ngram(order)
+
+
+def test_log_s_random_no_tags():
+    _test_log_s(_random_sentences(), None, None)
+
+
+def test_log_s_random_sos():
+    _test_log_s(_random_sentences(), "<s>", None)
+
+
+def test_log_s_random_eos():
+    _test_log_s(_random_sentences(), None, "</s>")
+
+
+def test_log_s_random_sos_eos():
+    _test_log_s(_random_sentences(), "<s>", "</s>")
+
+
+def _test_log_s(sentences, sos, eos):
+    lm_me = arpa.loadf(TEST_ARPA)[0]
+    lm_ken = kenlm.LanguageModel(TEST_ARPA)
+    results_me = []
+    results_ken = []
+    for sentence in sentences:
+        prob_me = lm_me.log_s(sentence, sos=sos, eos=eos)
+        scores_ken = lm_ken.full_scores(sentence, bool(sos), bool(eos))
+        prob_ken = sum(prob for prob, _, _ in scores_ken)
+        results_me.append(prob_me)
+        results_ken.append(prob_ken)
+    assert all(round(m - k, 4) == 0 for m, k in zip(results_me, results_ken))
+
+
+def _random_sentences():
+    sample = [" ".join(words) for words in nltk.corpus.udhr2.sents("eng.txt")]
+    return random.sample(sample, N_SENTENCES)
