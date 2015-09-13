e5024bfb23d741ee08696879b32e3b8d5df1949e
Stefan Fischer <sfischer13@ymail.com>
2015-09-13 16:08:16 +0200

    helper script for new tests

---
+++ tests/data/download.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+
+import nltk
+
+
+def main():
+    for corpus in ["punkt", "udhr2", "words"]:
+        nltk.download(corpus)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
