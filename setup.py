552458a39067a9fcdbe1cab9d7aefd05f475dcea
Stefan Fischer <sfischer13@ymail.com>
2017-06-05 03:11:57 +0200

    Reformat setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import os.path
+import sys
+
 from setuptools import find_packages
 from setuptools import setup
-import sys
 
 
 def read(name):
@@ -13,46 +14,46 @@ def read(name):
 
 
 install_requires = []
+
 if sys.version_info < (3, 4):
     install_requires += ["enum34"]
 
-
 setup(
-    name="arpa",
-    version="0.1.0b1",
-    author="Stefan Fischer",
-    author_email="sfischer13@ymail.com",
-    url="https://github.com/sfischer13/python-arpa/",
-    license="MIT",
-    description="This is a library for reading ARPA n-gram models.",
-    long_description=read("README.rst"),
-    keywords="ARPA n-gram ngram language model LM language technology LT "
-    "computational linguistics CL natural language processing NLP unigram bigram trigram",
-    install_requires=install_requires,
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest"],
-    packages=find_packages("src"),
-    package_dir={"": "src"},
+    author='Stefan Fischer',
+    author_email='sfischer13@ymail.com',
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Information Technology",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: Implementation",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        "Topic :: Software Development :: Libraries",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Text Processing",
-        "Topic :: Text Processing :: Linguistic"
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Linguistic'
     ],
+    description='Library for reading ARPA n-gram models.',
+    install_requires=install_requires,
+    keywords='ARPA n-gram ngram language model LM language technology LT '
+    'computational linguistics CL natural language processing NLP unigram bigram trigram',
+    license='MIT',
+    long_description=read('README.rst'),
+    name='arpa',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    url='https://github.com/sfischer13/python-arpa/',
+    version='0.1.0b1',
     zip_safe=True)
