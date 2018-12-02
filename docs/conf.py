be1ae756799a59cf0656c3bbaa536816eaf59cab
Stefan Fischer <sfischer13@ymail.com>
2018-12-02 19:30:04 +0100

    Support for Markdown in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -47,11 +47,16 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
